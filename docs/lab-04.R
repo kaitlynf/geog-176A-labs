19010707887718c90d446b77069f7213d9f5d842
Kaitlyn Fu <kaitlyn_fu@ucsb.edu>
2020-08-31 19:33:32 -0700

    lab 4

--- docs/lab-04.R
+++ docs/lab-04.R
@@ -6,6 +6,10 @@ library(sf)
 
 library(rmapshaper)
 
+library(readxl)
+
+dplyr::id()
+
 #step 1
 USAboundaries::us_counties() %>%
   filter(!state_name %in% c("Alaska", "Hawaii", "Puerto Rico")) %>%
@@ -66,3 +70,89 @@ plot_tess(t_grid, "triangulated tessellation")
 plot_tess(v_grid, "voroni tessellation")
 
 plot_tess(counties, "counties")
+
+#QUESTION 2
+
+tess_features = function(data, title){
+  df = data.frame(row.names = title,
+    mean_area = units::set_units((st_area(data)), "km^2") %>%
+                    units::drop_units() %>%
+                    head(n = 1),
+                  total_area = units::set_units((sum(st_area(data))), "km^2") %>%
+                    units::drop_units() %>%
+                    head(n = 1),
+                  n_features = count(hex_grid, "id") %>%
+                    st_drop_geometry() %>%
+                    select(n),
+                  standard_dev = sd(st_area(data)))
+  return(df)
+}
+
+sd(st_area(hex_grid))
+
+sd(c(hex_grid), na.rm = FALSE)
+
+tess_features(hex_grid, "hex grid summary")
+
+tess_summary = bind_rows(
+  tess_features(counties, "original counties summary"),
+  tess_features(sq_grid, "square grid summary"),
+  tess_features(hex_grid, "hexagonal grid summary"),
+  tess_features(v_grid_simp, "voroni tessellation summary"),
+  tess_features(t_grid_simp, "triangulated tessellation summary"))
+
+knitr::kable(tess_summary,
+             caption = "Tessellation Summary",
+             col.names = c("Mean Area", "Total Area", "Number of Features"))
+
+#Question 3
+
+read_excel("data/NID2019_U.xlsx") %>%
+  filter(!is.na(LATITUDE)) %>%
+  st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 4326) %>%
+  st_transform(5070) %>%
+  st_filter(counties) ->
+  NID
+
+#hex grid example
+st_join(hex_grid, NID) %>%
+  st_cast("MULTIPOLYGON") %>%
+  st_drop_geometry() %>%
+  count(COUNTY) %>%
+  setNames(c("COUNTY", "n")) %>%
+  left_join(hex_grid, by = COUNTY)
+
+point_in_polygon = function(points, polygon, group){
+  st_join(polygon, points) %>%
+    st_drop_geometry() %>%
+    count(get(group)) %>%
+    setNames(c(group, "n")) %>%
+    left_join(polygon, by = group) %>%
+    st_as_sf()
+}
+
+point_in_polygon(NID, hex_grid, "id") %>%
+  plot_pip()
+
+plot_pip = function(data, title){
+  ggplot() +
+    geom_sf(data = data, aes(fill = log(n)), col = NA) +
+    scale_fill_viridis_b() +
+    theme_void() +
+    labs(title = "Dam Locations",
+         caption = paste("Dams:", sum(data$n)))
+}
+
+unique(NID$PURPOSES) %>%
+  length
+
+NID %>%
+ filter(grepl("S", PURPOSES)) ->
+  water_supply
+
+NID %>%
+  filter(grepl("R", PURPOSES)) ->
+  recreation
+
+point_in_polygon(recreation, sq_grid, "id") %>%
+  plot_pip()
